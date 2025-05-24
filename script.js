7c0c4f7acfd00dbe26560efd17d5542d1290827f
chaurasiavipin <vipinchaurasia481@gmail.com>
2025-05-24 22:43:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 //your JS code here. If required.
+let submitbtm=document.getElementById("submit");
+let booklist=document.getElementById("book-list");
+  submitbtm.addEventListener("click", function(){
+	  let Title=document.getElementById("title").value.trim();
+	    let Author=document.getElementById("author").value.trim();
+	    let ISBN=document.getElementById("isbn").value.trim();
+
+	  if(Title=== ""||Author===""||ISBN===""){
+		  alert("Plese fill the blow");
+		  return
+	  }
+
+let row=document.createElement("tr");
+	  row.innerHTML='
+		  <td>${Title}</td>
+							  <td>${Author}</td>
+							  <td>${ISBN}</td>
+							  <td><button class="delete">Clear</button></td>
+		  
+	  ';
+							 
+	  booklist.apendChild(row);
+//agar sara like title author aur isbn ko fill kr diya gya to use khali kr do new line ke liye
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value = "";
+  });
+booklist.addEventListener("click", function(e){
+	if(e.target.classList.contains("delet")){
+		let row=e.target.closest("tr");
+		row.remove();
+	}
+	
+})
+
+
+
+
+
+
+
+
+
